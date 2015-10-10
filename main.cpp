6967c1c12d53dd21bf18203d3cfc5469ddba9841
Sven <second.little.hero@googlemail.com>
2015-10-10 14:13:10 +0200

    neuer branch commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
+void showIntro(){
+    cout << "hallo das ist der feature branch" <<endl;
+}
+
 int main()
 {
+
+    showIntro();
 
     for (int i=0;i<10;++i){
         cout << i*i << endl;
